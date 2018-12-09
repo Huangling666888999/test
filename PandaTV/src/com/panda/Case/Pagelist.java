789d1c40ee2deaabc8a71c608f02ba5f3c4bec76
fight <1397023553@qq.com>
2018-12-09 19:41:32 +0800

    addpandatv

---
+++ PandaTV/src/com/panda/Case/Pagelist.java
@@ -0,0 +1,12 @@
+package com.panda.Case;
+
+import java.util.Map;
+
+import com.panda.Commom.SystemUtils;
+
+public class Pagelist {
+public static void findbyid(){
+	Map<String, String> Control = SystemUtils.getPropertyMap("Config/Dataini.properties");
+	
+}
+}
